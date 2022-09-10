f225b1c1d00ff7b4603956000f039a2f97c885d6
RoniDeringer <ronideringer@gmail.com>
2022-09-10 09:17:21 -0300

    criado estrutura

---
+++ generics/src/main/java/tipagem_generica/Typetester.java
@@ -0,0 +1,29 @@
+package tipagem_generica;
+
+/**
+ *
+ * @author
+ * <https://stackoverflow.com/questions/3993982/how-to-check-type-of-variable-in-java>
+ */
+public class Typetester {
+
+    void printType(byte x) {
+        System.out.println(x + " is an byte");
+    }
+
+    void printType(int x) {
+        System.out.println(x + " is an int");
+    }
+
+    void printType(float x) {
+        System.out.println(x + " is an float");
+    }
+
+    void printType(double x) {
+        System.out.println(x + " is an double");
+    }
+
+    void printType(char x) {
+        System.out.println(x + " is an char");
+    }
+}
