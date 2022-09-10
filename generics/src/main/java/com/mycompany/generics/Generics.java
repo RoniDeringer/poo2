4f96cc65f735484abd2d19fd1e9a6d2d0660b90e
RoniDeringer <ronideringer@gmail.com>
2022-09-10 08:37:49 -0300

    create new folder - generics

---
+++ generics/src/main/java/com/mycompany/generics/Generics.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.generics;
+
+/**
+ *
+ * @author Roni
+ */
+public class Generics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
