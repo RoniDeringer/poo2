ba76d61bd8d0832d4c045c9b7eddf971bfd02516
RoniDeringer <ronideringer@gmail.com>
2022-09-10 09:13:52 -0300

    criado estrutura

---
+++ generics/src/main/java/tipagem_generica/MeuString.java
@@ -0,0 +1,19 @@
+package tipagem_generica;
+
+/**
+ *
+ * @author Roni Deringer <ronideringer.ifc@gmail.com>
+ */
+public class MeuString {
+
+    private String str;
+    public String dado;
+        
+    public MeuString(String str) {
+        str = str;
+    }
+
+    public String getStr() {
+        return str;
+    }
+}
